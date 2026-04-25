38cd9fc92158a7cae3a9e30fc50fce426975c0a9
Nithin <nithin.ch9963@gmail.com>
2026-04-25 18:11:37 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+  const filePath = path.join(__dirname, 'public', 'index.html');
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.writeHead(500);
+      res.end('Error loading page');
+      return;
+    }
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(data);
+  });
+});
+
+server.listen(3000, () => {
+  console.log('Portfolio running on port 3000');
+});
